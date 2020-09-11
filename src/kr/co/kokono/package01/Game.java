d428ddc7e40269f93bf85ab56f59a050e923f4d4
kooprogramming <hsg9984@gmail.com>
2020-09-11 14:38:53 +0900

    Initial commit

---
+++ src/kr/co/kokono/package01/Game.java
@@ -0,0 +1,16 @@
+package kr.co.kokono.package01;
+
+public class Game {
+
+    UI ui = new UI();
+
+    public static void main(String[] args) {
+
+        new Game();
+    }
+
+    public Game() {
+
+        ui.createUI();
+    }
+}
---
+++ src/kr/co/kokono/package01/UI.java
@@ -0,0 +1,57 @@
+package kr.co.kokono.package01;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UI {
+
+    JFrame window;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    JButton startButton;
+
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
+
+    public void createUI() {
+
+        //WINDOW
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black);
+        window.setLayout(null);
+
+
+        // TITLE SCREEN
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 500);
+        titleNamePanel.setBackground(Color.black);
+
+        titleNameLabel = new JLabel("ADVENTURE");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+
+        titleNamePanel.add(titleNameLabel);
+
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(normalFont);
+        startButton.setFocusPainted(false);
+
+        startButtonPanel.add(startButton);
+
+        ////////////////////////////////////////////////
+
+        window.add(titleNamePanel);
+        window.add(startButtonPanel);
+
+        window.setVisible(true);
+    }
+}
